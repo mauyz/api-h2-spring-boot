f65c1f11857a94a917c8e88ca01a57bc98b165ea
Mauyz <tsiorymauyz@gmail.com>
2024-03-12 15:55:17 +0300

    init project

---
+++ src/main/java/com/mauyz/apiwithh2/ApiWithH2Application.java
@@ -0,0 +1,13 @@
+package com.mauyz.apiwithh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWithH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWithH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mauyz/apiwithh2/ApiWithH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauyz.apiwithh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWithH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
