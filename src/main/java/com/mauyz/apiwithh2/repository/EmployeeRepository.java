e460b256e9e0358f98ef43ce2167abccea3a1787
mauyz <tsiorymauyz@gmail.com>
2024-03-22 15:18:51 +0300

    Extends EmployeeRepository from JpaRepository

--- src/main/java/com/mauyz/apiwithh2/repository/EmployeeRepository.java
+++ src/main/java/com/mauyz/apiwithh2/repository/EmployeeRepository.java
@@ -1,11 +1,11 @@
 package com.mauyz.apiwithh2.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.mauyz.apiwithh2.model.Employee;
 
 @Repository
-public interface EmployeeRepository extends CrudRepository<Employee, Long>{
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
