e51626b18b36f1f7b2ee69e83f30326884dd9d3a
mauyz <tsiorymauyz@gmail.com>
2024-03-22 15:57:36 +0300

    Refractors api service

--- src/main/java/com/mauyz/apiwithh2/controller/EmployeeController.java
+++ src/main/java/com/mauyz/apiwithh2/controller/EmployeeController.java
@@ -1,22 +1,54 @@
 package com.mauyz.apiwithh2.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.mauyz.apiwithh2.model.Employee;
 import com.mauyz.apiwithh2.service.EmployeeService;
+import com.mauyz.apiwithh2.util.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
 public class EmployeeController {
 
-    @Autowired
-    private EmployeeService employeeService;
+    private final EmployeeService employeeService;
 
-    
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+
     @GetMapping("/employees")
-    public Iterable<Employee> getEmployees() {
+    public ResponseEntity<ApiResponse<?>> getEmployees() {
         return employeeService.getEmployees();
     }
 
+    @PostMapping("/employees")
+    public ResponseEntity<ApiResponse<?>> saveEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<ApiResponse<?>> deleteEmployee(@PathVariable("id") Long id) {
+        return employeeService.deleteEmployee(id);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public  ResponseEntity<?> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception
+    ) {
+        var errors = new HashMap<String, String>();
+        exception.getBindingResult()
+                .getAllErrors()
+                .forEach(error -> {
+                    var fieldName = ((FieldError)error).getField();
+                    var errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName, errorMessage);
+                });
+        return  new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 }
--- src/main/java/com/mauyz/apiwithh2/service/EmployeeService.java
+++ src/main/java/com/mauyz/apiwithh2/service/EmployeeService.java
@@ -1,37 +1,50 @@
 package com.mauyz.apiwithh2.service;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.mauyz.apiwithh2.model.Employee;
 import com.mauyz.apiwithh2.repository.EmployeeRepository;
-
+import com.mauyz.apiwithh2.util.ApiResponse;
+import com.mauyz.apiwithh2.util.ResponseBuilder;
 import lombok.Data;
-import lombok.NonNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 @Data
 @Service
 public class EmployeeService {
 
-    @Autowired
-    private EmployeeRepository employeeRepository;
+    private final EmployeeRepository employeeRepository;
+    private final ResponseBuilder responseBuilder;
 
-    public Optional<Employee> findEmployee(@NonNull final Long id) {
-        return employeeRepository.findById(id);
+    public EmployeeService(EmployeeRepository employeeRepository, ResponseBuilder responseBuilder) {
+        this.employeeRepository = employeeRepository;
+        this.responseBuilder = responseBuilder;
     }
 
-    public Iterable<Employee> getEmployees() {
-        return employeeRepository.findAll();
+    public ResponseEntity<ApiResponse<?>> findEmployee(final Long id) {
+        final Employee employee = employeeRepository.findById(id).orElse(null);
+        if (employee == null) {
+            return responseBuilder.buildResponse(HttpStatus.NOT_FOUND.value(), "Not found");
+        }
+        return responseBuilder.buildResponse(HttpStatus.OK.value(), "", employee);
     }
 
-    public void deleteEmployee(@NonNull final Long id) {
-        employeeRepository.deleteById(id);
+    public ResponseEntity<ApiResponse<?>> getEmployees() {
+        Iterable<Employee> list = employeeRepository.findAll();
+        return responseBuilder.buildResponse(HttpStatus.OK.value(), "", list);
     }
 
-    public Employee saveEmployee(@NonNull Employee employee) {
+    public ResponseEntity<ApiResponse<?>> deleteEmployee(final Long id) {
+        try {
+            employeeRepository.deleteById(id);
+            return responseBuilder.buildResponse(HttpStatus.OK.value(), "");
+        } catch (IllegalArgumentException e) {
+            return responseBuilder.buildResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+        }
+    }
+
+    public ResponseEntity<ApiResponse<?>> saveEmployee(Employee employee) {
         Employee savedEmployee = employeeRepository.save(employee);
-        return savedEmployee;
+        return responseBuilder.buildResponse(HttpStatus.OK.value(), "", savedEmployee);
     }
 }
