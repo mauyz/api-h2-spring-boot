31b16fa67a75f18376f7b50cec00018c5c2501c0
mauyz <tsiorymauyz@gmail.com>
2024-03-22 15:17:21 +0300

    set Employee fields not nullable

--- src/main/java/com/mauyz/apiwithh2/model/Employee.java
+++ src/main/java/com/mauyz/apiwithh2/model/Employee.java
@@ -17,14 +17,15 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "first_name")
+    @Column(nullable = false)
     private String firstName;
 
-    @Column(name = "last_name")
+    @Column(nullable = false)
     private String lastName;
 
+    @Column(nullable = false)
     private String mail;
-
+    @Column(nullable = false)
     private String password;
 
 
